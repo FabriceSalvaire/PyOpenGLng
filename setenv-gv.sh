6eb9a9a6c679ae5fd328f062db03211ce1d3b8b4
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-01-10 00:56:07 +0100

    removed scripts

--- setenv-gv.sh
+++
@@ -1,9 +0,0 @@
-set_gcc47
-set_python27
-append_to_python_path_if_not $PWD
-
-append_to_ld_library_path_if_not /opt/gv/local/stow/freetype-2.5.2/lib
-append_to_python_path_if_not ~/dev/gvlab/measure-tool-dev/third-party
-
-# End
-
